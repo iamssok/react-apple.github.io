e857ec831d957cc178138bb06b4f880a6383a8e2
sso <iamssok@naver.com>
2024-08-29 21:45:19 +0900

    for deploy

--- src/index.jsx
+++ src/index.jsx
@@ -151,7 +151,7 @@ const GlobalStyle = createGlobalStyle`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <GlobalStyle />
       <App />
